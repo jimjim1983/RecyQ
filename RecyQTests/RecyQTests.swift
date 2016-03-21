cf8b26f6e4437b13a358836869b8cd20a01230aa
Jim Petri <jimjim1983@hotmail.com>
2016-03-21 13:43:02 +0100

    Initial Commit

---
+++ RecyQTests/RecyQTests.swift
@@ -0,0 +1,36 @@
+//
+//  RecyQTests.swift
+//  RecyQTests
+//
+//  Created by Jim Petri on 21/03/16.
+//  Copyright © 2016 Jimsalabim. All rights reserved.
+//
+
+import XCTest
+@testable import RecyQ
+
+class RecyQTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
