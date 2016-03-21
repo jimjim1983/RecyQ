a7178cf28ff48fc9666531aee3f0e0d0f6558fe4
Alyson Vivattanapa <alyson.vivattanapa@gmail.com>
2016-03-21 14:38:38 +0100

    Hello, world! GitHub test.

--- RecyQ/FirstViewController.swift
+++ RecyQ/FirstViewController.swift
@@ -12,6 +12,8 @@ class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("Hello, world!")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
