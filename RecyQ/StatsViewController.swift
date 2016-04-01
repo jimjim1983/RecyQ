cc6d836c0b398f24339fca8fff0da237da54267b
Alyson Vivattanapa <alyson.vivattanapa@gmail.com>
2016-04-01 19:05:05 +0200

    Got the activity indicator working, and when stats view controller appears again (thanks to Enrico who is amazing) the scroll view goes back to the top.

--- RecyQ/StatsViewController.swift
+++ RecyQ/StatsViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class StatsViewController: UIViewController {
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var paperView: UIView!
+    @IBOutlet weak var plasticView: UIView!
+    @IBOutlet weak var textileView: UIView!
+    @IBOutlet weak var ijzerView: UIView!
+    @IBOutlet weak var ewasteView: UIView!
+    @IBOutlet weak var biowasteView: UIView!
     @IBOutlet var usernameLabel: UILabel!
 //    @IBOutlet var userIDLabel: UILabel!
     @IBOutlet var plasticLabel: UILabel!
@@ -61,10 +69,70 @@ class StatsViewController: UIViewController {
         if let bioWaste = testUser.amountOfBioWaste {
             bioWasteLabel.text = "\(bioWaste)"
         }
-        
-        
 
-
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        scrollView.setContentOffset(CGPointMake(0,0), animated: true)
+        
+        activityIndicator.startAnimating()
+        
+        paperView.alpha = 0
+        plasticView.alpha = 0
+        textileView.alpha = 0
+        ijzerView.alpha = 0
+        ewasteView.alpha = 0
+        biowasteView.alpha = 0
+        
+        slidePaperView()
+        
+        NSTimer.scheduledTimerWithTimeInterval(0.25, target: self, selector: "slidePlasticView", userInfo: nil, repeats: false)
+        
+         NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: "slideTextileView", userInfo: nil, repeats: false)
+        
+         NSTimer.scheduledTimerWithTimeInterval(0.75, target: self, selector: "slideIjzerView", userInfo: nil, repeats: false)
+        
+         NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "slideEwasteView", userInfo: nil, repeats: false)
+        
+         NSTimer.scheduledTimerWithTimeInterval(1.25, target: self, selector: "slideBiowasteView", userInfo: nil, repeats: false)
+        
+    }
+    
+    
+    func slidePaperView() {
+         addSlideAnimation(paperView)
+    }
+    
+    func slidePlasticView() {
+        addSlideAnimation(plasticView)
+    }
+    
+    func slideTextileView() {
+        addSlideAnimation(textileView)
+    }
+    
+    func slideIjzerView() {
+        addSlideAnimation(ijzerView)
+    }
+    
+    func slideEwasteView() {
+        addSlideAnimation(ewasteView)
+    }
+    
+    func slideBiowasteView() {
+        addSlideAnimation(biowasteView)
+        activityIndicator.stopAnimating()
+    }
+    
+    func addSlideAnimation(viewName: UIView) {
+        let animation = CABasicAnimation(keyPath: "transform.translation.x")
+        animation.fromValue = -500
+        animation.toValue = 0
+        animation.duration = 1
+        animation.autoreverses = false
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
+        viewName.layer.addAnimation(animation, forKey: nil)
+        viewName.alpha = 1
     }
 
 }
