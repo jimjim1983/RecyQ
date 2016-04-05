a6d3698cc0b682483fe1343b04b52bc2bc1709ff
Alyson Vivattanapa <alyson.vivattanapa@gmail.com>
2016-04-05 18:13:50 +0200

    Wrote some pseudo code for Store Detail View Controller.

--- RecyQ/StoreDetailViewController.swift
+++ RecyQ/StoreDetailViewController.swift
@@ -16,6 +16,7 @@ class StoreDetailViewController: UIViewController {
     @IBOutlet var descriptionLabel: UILabel!
     @IBOutlet var image: UIImageView!
     @IBOutlet var logo: UIImageView!
+    @IBOutlet weak var redeemButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,5 +32,23 @@ class StoreDetailViewController: UIViewController {
     func navigateBack() {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    @IBAction func redeemToken(sender: UIButton) {
+        
+    let statsViewController = StatsViewController()
+    var startNumberOfTokens = statsViewController.testUser.amountOfTokens
+    // var subtractor = cost of item
+    // var endNumberOfTokens = int
+    // let endNumberOfTokens = startNumberOfTokens - subtractor
+    //  statsViewController.testUser.amountOfTokens = endNumberOfTokens
+    // code that goes to backend to show how many tokens were redeemed and for which product - should go to user and general coupon table item
+    // code that goes to new custom pop up view that says token has been successfully redeemed.
+    // option to see coupon in profile view controller
+    // option to dismiss
+    // redeem button restored? or back to store view controller?
+    
+    }
+  
+    
     
 }
