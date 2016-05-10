7111d2105b936892a134c71f2ebcc4a6117387a3
Jim Petri <jimjim1983@hotmail.com>
2016-05-10 10:55:34 +0200

    Fixed the array problem where it would keep adding entries to the array

--- RecyQ/ProfileViewController.swift
+++ RecyQ/ProfileViewController.swift
@@ -61,6 +61,7 @@ class ProfileViewController: UIViewController, MKMapViewDelegate, UITableViewDel
     }
     
     override func viewWillAppear(animated: Bool) {
+        self.couponItems.removeAll()
                  emailLabel.text = user?.name
         // go trough all coupons and find the one with the same user uid, then add them to the array for the tableview
         self.couponsRef.queryOrderedByChild("uid").queryEqualToValue(user?.uid).observeEventType(.Value, withBlock: { snapshot in
