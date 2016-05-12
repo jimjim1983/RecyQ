b69077b67570823c8be87d1652bd59aeb4c115d2
Alyson Vivattanapa <alyson.vivattanapa@gmail.com>
2016-05-12 22:22:04 +0200

    Refactored getAmountOfWaste code.

--- RecyQ/LeaderboardViewController.swift
+++ RecyQ/LeaderboardViewController.swift
@@ -45,7 +45,44 @@ class LeaderboardViewController: UIViewController, UITableViewDelegate, UITableV
     }
     
     override func viewWillAppear(animated: Bool) {
-//       getAmountOfWaste()
+       getAmountOfWaste()
+//
+//        self.clientsRef.queryOrderedByChild("amountOfBioWaste").observeEventType(.Value, withBlock: { snapshot in
+//            
+//            
+//            if let snapshots = snapshot.children.allObjects as? [FDataSnapshot] {
+//                
+//                self.snapshotArray = snapshots
+//                
+//                for item in self.snapshotArray {
+//                    
+//                    let amountOfPlastic = item.value.objectForKey("amountOfPlastic") as? Double
+//                    let amountOfBioWaste = item.value.objectForKey("amountOfBioWaste") as? Double
+//                    let amountOfEWaste = item.value.objectForKey("amountOfEWaste") as? Double
+//                    let amountOfIron = item.value.objectForKey("amountOfIron") as? Double
+//                    let amountOfPaper = item.value.objectForKey("amountOfPaper") as? Double
+//                    let amountOfTextile = item.value.objectForKey("amountOfTextile") as? Double
+//                    
+//                    let wasteAmount = amountOfPlastic! + amountOfBioWaste! + amountOfEWaste! + amountOfIron! + amountOfPaper! + amountOfTextile!
+//                    
+//                    if let username = item.value.objectForKey("name") as? String {
+//                        
+//                        self.wasteDictionary["\(username)"] = wasteAmount
+//                    }
+//                }
+//                
+//                self.tableView.reloadData()
+//                
+//            }
+//            
+//            print(self.wasteDictionary)
+//            
+//        })
+
+        
+    }
+    
+    func getAmountOfWaste() {
         
         self.clientsRef.queryOrderedByChild("amountOfBioWaste").observeEventType(.Value, withBlock: { snapshot in
             
@@ -66,55 +103,18 @@ class LeaderboardViewController: UIViewController, UITableViewDelegate, UITableV
                     let wasteAmount = amountOfPlastic! + amountOfBioWaste! + amountOfEWaste! + amountOfIron! + amountOfPaper! + amountOfTextile!
                     
                     if let username = item.value.objectForKey("name") as? String {
-                        
+                    
                         self.wasteDictionary["\(username)"] = wasteAmount
                     }
                 }
-                
                 self.tableView.reloadData()
-                
             }
             
             print(self.wasteDictionary)
             
         })
-
-        
     }
     
-//    func getAmountOfWaste() {
-//        
-//        self.clientsRef.queryOrderedByChild("amountOfBioWaste").observeEventType(.Value, withBlock: { snapshot in
-//            
-//            
-//            if let snapshots = snapshot.children.allObjects as? [FDataSnapshot] {
-//                
-//                self.snapshotArray = snapshots
-//                
-//                for item in self.snapshotArray {
-//                    
-//                    let amountOfPlastic = item.value.objectForKey("amountOfPlastic") as? Double
-//                    let amountOfBioWaste = item.value.objectForKey("amountOfBioWaste") as? Double
-//                    let amountOfEWaste = item.value.objectForKey("amountOfEWaste") as? Double
-//                    let amountOfIron = item.value.objectForKey("amountOfIron") as? Double
-//                    let amountOfPaper = item.value.objectForKey("amountOfPaper") as? Double
-//                    let amountOfTextile = item.value.objectForKey("amountOfTextile") as? Double
-//                    
-//                    let wasteAmount = amountOfPlastic! + amountOfBioWaste! + amountOfEWaste! + amountOfIron! + amountOfPaper! + amountOfTextile!
-//                    
-//                    if let username = item.value.objectForKey("name") as? String {
-//                    
-//                        self.wasteDictionary["\(username)"] = wasteAmount
-//                    }
-//                }
-//                
-//            }
-//            
-//            print(self.wasteDictionary)
-//            
-//        })
-//    }
-//    
 
     
         func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
